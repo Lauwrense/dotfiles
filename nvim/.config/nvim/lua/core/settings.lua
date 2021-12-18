edcdf30352fd99bc72e8904f75ba4cfe6df9e125
Tenwasa <tenwasalaw@gmail.com>
2021-12-18 15:38:45 +0800

    Neovim config edited

---
+++ nvim/.config/nvim/lua/core/settings.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.termguicolors = true
+
+opt.smartcase = true
+opt.list = true
+opt.listchars = "tab:> ,trail:•"
+
+opt.mouse = "a"
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.wrap = false
+opt.splitright = true
